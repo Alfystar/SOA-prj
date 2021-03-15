6b32fb0c259b74d545e15a0bbd2d17a7e49b5738
alfystar <alfystar1701@gmail.com>
2021-03-15 23:05:17 +0100

    reorder code and factorize functionality

--- include/sysCall_Discovery.h
+++ include/sysCall_Discovery.h
@@ -3,30 +3,28 @@
 
 #include "globalDef.h"
 
-
 #define EXPORT_SYMTAB
-#include <linux/module.h>
-#include <linux/kernel.h>
-#include <linux/fs.h>
+#include "vtpmo.h"
+#include <asm/apic.h>
+#include <asm/cacheflush.h>
+#include <asm/page.h>
 #include <linux/cdev.h>
-#include <linux/errno.h>
 #include <linux/device.h>
+#include <linux/errno.h>
+#include <linux/fs.h>
+#include <linux/interrupt.h>
+#include <linux/kernel.h>
 #include <linux/kprobes.h>
-#include <linux/mutex.h>
 #include <linux/mm.h>
+#include <linux/module.h>
+#include <linux/mutex.h>
 #include <linux/sched.h>
 #include <linux/slab.h>
-#include <linux/version.h>
-#include <linux/interrupt.h>
-#include <linux/time.h>
 #include <linux/string.h>
-#include <linux/vmalloc.h>
-#include <asm/page.h>
-#include <asm/cacheflush.h>
-#include <asm/apic.h>
 #include <linux/syscalls.h>
-#include "vtpmo.h"
-
+#include <linux/time.h>
+#include <linux/version.h>
+#include <linux/vmalloc.h>
 
 extern unsigned long *hacked_ni_syscall;
 extern unsigned long **hacked_syscall_tbl;
@@ -36,7 +34,6 @@ extern unsigned long sys_ni_syscall_address;
 
 #define MAX_FREE 15
 extern int free_entries[MAX_FREE];
-
 
 int foundFree_entries(void);
 
